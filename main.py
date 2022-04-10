3a29e249465691c5811cb14c59be46cc244db333
AndreyTheHacker <andrejpavlenko666@gmail.com>
2022-04-10 15:58:48 +0900

    Status for every gif, Duplicate renamer

--- main.py
+++ main.py
@@ -2,16 +2,36 @@ import aiohttp as aih
 import asyncio
 import sys
 import json
+import random
 import aiofiles
 
+works = []
+
 async def download(session,url,name):
-    print("Starting download "+name)
+    #print("Starting download "+name)
+    if name in works:
+        name=name.split(".")[0]+str(random.randint(88888,777777))+"."+name.split(".")[-1]
+    works.append(name)
+    chunksize = 8192
+    await asyncio.sleep(0.5)
     async with session.get(url) as resp:
         if resp.status == 200:
             f = await aiofiles.open(name, mode='wb')
-            await f.write(await resp.read())
+            load = 0
+            while True:
+                loaded = (load/(1024**2))
+                print("\033["+str(works.index(name))+";0"+"H", end='')
+                print("%-30s => %.3fMB"%(name, loaded), end='\r')
+                #print(works.index(name))
+                chunk = await resp.content.read(chunksize)
+                if not chunk: break
+                await f.write(chunk)
+                load += chunksize
             await f.close()
-            print("Download of "+name+" OKAY!")
+            works.remove(name)
+            #print("\033[2J")
+            print(" "*38, end='')
+            #print("Download of "+name+" OKAY!")
 
 async def getrawdata():
     async with aih.ClientSession() as ses:
@@ -44,6 +64,7 @@ async def main():
     async with aih.ClientSession() as session:
         print("Getting ready...")
         tasks = []
+        print("\033[2J")
         for url in urls:
             tasks.append(asyncio.ensure_future(download(session, url, url.split("/")[-1])))
 
